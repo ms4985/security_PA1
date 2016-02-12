69f7ad1ae0c9ee9462c96618b05fd859f9c23cc5
Megan Skrypek <ms4985@columbia.edu>
2016-02-11 22:05:37 -0500

    client encrypts, sends file, and server decrypts correctly

--- client.py
+++ client.py
@@ -2,9 +2,17 @@ from Crypto.Cipher import AES
 import socket
 import sys
 import select
+from os import urandom
 
 #globals
 SIZE = 4096
+BLOCK_SIZE = 16
+
+#CHANGE WHEN ADDING REST OF INPUT ARGS
+if len(sys.argv) < 3:
+	print 'ERROR: not enough arguments'
+	sys.exit()
+
 
 #handle user inputs
 host, port = sys.argv[1], int(sys.argv[2])
@@ -12,25 +20,38 @@ if ((port < 1024) or (port > 49151)):
 	print 'ERROR: invalid port'
 	sys.exit
 
-key = 'a1b2c3d4e5f6g7h8'
+key = 'a1b2c3d4e5f6g7h8' #sys.argv[3]
 if len(key) != 16:
 	print 'ERROR: password not 16 characters'
 	sys.exit()
 
-fname = 'file1.txt'
-
+fname = 'file1.txt' #sys.argv[4]
+ename = 'en' + fname
 
 #set up AES encryption
-iv = 16 * '\x00'
+iv = urandom(BLOCK_SIZE)
 mode = AES.MODE_CBC
 
 #function for encrypting file using AES in CBC mode
-def encrypt_file(fname, key):
+def encrypt_file():
 	encryptor = AES.new(key, mode, IV=iv)
-	text = 'a' * 64 
-	print text
-	ciphertext = encryptor.encrypt(text)
-	print ciphertext
+	
+	#read file in rb mode
+	with open(fname, 'rb') as f:
+		#open file to write encrypted file in wb mode
+		with open(ename, 'wb') as e:
+			#prepend the iv string to the ciphertext
+			e.write(iv)
+			stop = False
+			while stop == False:
+				segment = f.read(BLOCK_SIZE)
+				#check if reached EOF
+				if len(segment) == 0:
+					stop = True
+				#check if we need to pad the last segment
+				elif len(segment) < 16:
+					segment += ' '*(16-len(segment))
+				e.write(encryptor.encrypt(segment))
 
 #set up client socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -43,6 +64,13 @@ except:
 	print "no server on host/port"
 	sys.exit()
 
+def send_file(sock):
+	with open(ename, 'rb') as f:
+		data = f.read(SIZE)
+		sock.send(data)
+
+sent = False
+
 #handle data sent from server
 while 1:
 	sockets = [sys.stdin, client]
@@ -52,9 +80,13 @@ while 1:
 			if sock == client:
 				data = sock.recv(SIZE)
 				if data:
-					sys.stdout.write(data)
-					sys.stdout.write('\n* ')
-					sys.stdout.flush()
+					print data
+				if sent == False:
+					encrypt_file()
+					sock.send('file')
+					send_file(sock)
+					sent == True
+
 				#exit if server quit
 				else:
 					print "server disconnected"
@@ -64,9 +96,10 @@ while 1:
 				msg = sys.stdin.readline()
 				if msg:
 					client.send(msg)
-				sys.stdout.write('* ')
-				sys.stdout.flush()
 
 	#catch crtl-c interrupts
 	except (KeyboardInterrupt, SystemExit):
+		client.send('close')
+		client.close()
 		sys.exit()
+
--- server.py
+++ server.py
@@ -1,11 +1,19 @@
-import Crypto
+from Crypto.Cipher import AES
 import socket
 import sys
 import select
 
+#globals
 SIZE = 4096
+BLOCK_SIZE = 16
 connections = []
+mode = AES.MODE_CBC
+
+#handle user input
 host, port = '', int(sys.argv[1])
+if ((port < 1024) or (port > 49151)):
+	print 'ERROR: invalid port'
+	sys.exit
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host,port))
@@ -15,11 +23,45 @@ print "server listening for clients..."
 connections.append(server)
 
 def handle_client(sock, address):
-	print 'handle client'
-	sock.sendto("enter password", address)
-	password = sock.recv(SIZE)
-	print password
+	try:
+		try:
+			data = sock.recv(SIZE)
+			if data:
+				print data
+			if data == 'close':
+				connections.remove(sock)
+				sock.close()
+			if data == 'file':
+				handle_file(sock)
+				decrypt_file()
+		except:
+			close(sock)
+	except:
+		pass
 
+def handle_file(sock):
+	with open('encfile', 'wb') as f:
+		data = sock.recv(SIZE)
+		f.write(data)
+	print 'received file'
+
+def decrypt_file():
+	key = 'a1b2c3d4e5f6g7h8'
+	with open('encfile', 'rb') as e:
+		iv = e.read(BLOCK_SIZE)
+		decryptor = AES.new(key, mode, iv)
+		with open('decfile', 'wb') as d:
+			stop = False
+			while stop == False:
+				segment = e.read(BLOCK_SIZE)
+				if len(segment) == 0:
+					stop == True
+				d.write(decryptor.decrypt(segment))	
+
+def close(socket):
+	print 'close'
+	connections.remove(socket)
+	socket.close()
 
 try:
 	while 1:
@@ -28,9 +70,12 @@ try:
 			if sock == server:
 				socket, address = server.accept()
 				socket.sendto("connected!", address)
-				handle_client(socket, address)
+				connections.append(socket)
 			else:
-				print 'in else block'
+				try:
+					handle_client(socket, address)
+				except:
+					close(sock)
 
 except (KeyboardInterrupt, SystemExit):
 	print "\n server shutting down..."
